11ba226c05b60c8b89ed9414b18c4127b210aed6
Billy Mumby <billy.mumby@outlook.com>
2020-07-16 13:56:41 +0100

    first commit

---
+++ FluentHttpClient/FluentHttpClient/IHttpClient.cs
@@ -0,0 +1,7 @@
+namespace FluentHttpClient
+{
+    public interface IHttpClient
+    {
+        
+    }
+}
---
+++ dotnet-wrappers/src/DotNetWrappers/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DotNetWrappers
+{
+    public class Class1
+    {
+    }
+}
